cd02b8780629db10722292a9503a0b31ec308ae8
debdi55 <144518137+debdi55@users.noreply.github.com>
2025-02-05 18:15:09 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -11,7 +11,7 @@ public class Hello {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
     }
     
 }
