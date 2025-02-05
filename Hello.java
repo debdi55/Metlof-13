feab2658dc0a88e01d209fe07dde1cc3b4a3b81b
debdi55 <144518137+debdi55@users.noreply.github.com>
2025-02-05 15:06:40 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+package hello;
+
+/**
+ *
+ * @author Pc
+ */
+public class Hello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
